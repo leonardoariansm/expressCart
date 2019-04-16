cc4629dfd03fcd37be1980be50643866737345f7
Rishabh Tiwari <rishabh.t@media.net>
2019-04-16 10:40:05 +0530

    fixed size image

--- classes/services/FileServices.js
+++ classes/services/FileServices.js
@@ -5,7 +5,10 @@ const config = require('config');
 const promise = require('bluebird');
 const mime = require('mime-type/with-db');
 const Enums = require('../models/Enums');
+const sharp = require('sharp');
 const{ProductDataStores} = require('../DataStores/ProductDataStores');
+const IMAGE_HEIGHT = config.get('products.imageHeight');
+const IMAGE_WIDTH = config.get('products.imageWidth');
 
 const uploadDir = config.get('products.productImageUploadDir');
 
@@ -56,7 +59,7 @@ class FileServices{
             const productPath = product.productPermalink;
             const uploadDir = path.join('public', 'uploads', productPath);
             common.checkDirectorySync(uploadDir);
-            let source = fs.createReadStream(file.path);
+            let source = sharp(file.path).resize({height: IMAGE_HEIGHT, width: IMAGE_WIDTH});
             let dest = fs.createWriteStream(path.join(uploadDir, file.originalname.replace(/ /g, '_')));
             source.pipe(dest);
             let result = await new promise((resolve, reject) => {
